06f6cf3362e37040d0d6ada1bf22771963548c2d
MatejValocký <matej.valocky@gmail.com>
2022-04-23 18:50:47 +0200

    pridanie query efektov

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
 // JQUERY PRE BANNER
 
 $(function(){
-   $(".banner").fadeOut(2000);
-   $(".banner").fadeIn(2000);
+   
+   $(".banner").hide().fadeIn(2000);
+   
+   $(".aboutImg0").hide().fadeIn(4000);
+   $(".aboutImg1").hide().fadeIn(5000);
+   $(".imageAbout").hide().fadeIn(3000);
+   $(".prveMenu").hide().fadeIn(500);
+   $(".druheMenu").hide().fadeIn(1000);
+   $(".tretieMenu").hide().fadeIn(1500);
+   $(".stvrteMenu").hide().fadeIn(2000);
+   $(".piateMenu").hide().fadeIn(2500);
+   $(".siesteMenu").hide().fadeIn(3000);
+
+
+   $(".utulokLogo").hide().fadeIn(3000);
+   $(".headerLogo").hide().fadeIn(500);
+   $("#nekupuj").hide().fadeIn(5000);
+
+
+   
 
 
 });
-
 
 
