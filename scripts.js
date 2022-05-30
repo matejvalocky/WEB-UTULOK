a6da166830591977e47b90a597f69df41c1b164f
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2022-05-31 00:33:52 +0200

    nastavenie jQuery plynulych prechodov po kliknutí do príslušnách sekcii, okrem sekcie kontakt a šípky nahor

--- scripts.js
+++ scripts.js
@@ -20,10 +20,64 @@ $(function(){
    $(".headerLogo").hide().fadeIn(500);
    $("#nekupuj").hide().fadeIn(5000);
 
-   $(".slideshow-container").delay(1000).fadeIn(500);
+   $(".slideshow-container").delay(1000).fadeIn(500);     //  OPOZDENIE ANIMACIE BANNERA O JEDNU SEKUNDU PRI STARTE STRANKY  
+                          
+
+
+
+   $(".prveMenu").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#oUtulku").offset().top
+    }, 2000);
+});
+
+
+
+
+$(".druheMenu").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#oAdopcii").offset().top
+  }, 2000);
+});
 
 
    
+
+$(".tretieMenu").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#vencenie").offset().top
+  }, 2000);
+});
+
+
+
+$(".stvrteMenu").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#pravidla").offset().top
+  }, 2000);
+});
+
+
+
+
+$(".piateMenu").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#naAdopciu").offset().top
+  }, 2000);
+});
+
+
+
+
+$("siesteMenu").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#kontakt").offset().top
+  }, 2000);
+});
+
+
+
+
 
 
 });
